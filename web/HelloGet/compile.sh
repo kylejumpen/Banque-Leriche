47f9b5125502f81b7ba51b80eb0e6de955509f4b
Quentin <quentin.lerebours@insa-rouen.fr>
2016-04-12 20:42:56 +0200

    Restructuration !

--- web/HelloGet/compile.sh
+++
@@ -1,7 +0,0 @@
-CLASSPATH=./src:$JBOSS_HOME/modules/system/layers/base/javax/servlet/api/main/jboss-servlet-api_3.1_spec-1.0.0.Final.jar
-
-
-javac -cp $CLASSPATH -sourcepath src -d ./WEB-INF/classes src/*.java
-
-jar cf HelloGet.war WEB-INF helloGet.html
-cp HelloGet.war $JBOSS_HOME/standalone/deployments
