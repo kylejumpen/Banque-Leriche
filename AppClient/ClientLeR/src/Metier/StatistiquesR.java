9dd2643f5533ef755adda5d05a11802dfbbdf675
pauline <paulinemouches@gmail.com>
2016-05-07 18:26:22 +0200

    maj stats

---
+++ AppClient/ClientLeR/src/Metier/StatistiquesR.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Metier;
+
+/**
+ *
+ * @author pauline
+ */
+public class StatistiquesR extends CoRest {
+
+    public String getNbClients() {
+        target = client.target(baseUrl + "/stats/clients");
+        response = target.request().get();
+        String rep = response.readEntity(String.class);
+        response.close();
+        return rep;
+    }
+
+    public String getNbOperations() {
+        target = client.target(baseUrl + "/stats/operations");
+        response = target.request().get();
+        String rep = response.readEntity(String.class);
+        response.close();
+        return rep;
+    }
+
+    public String getNbComptes() {
+        target = client.target(baseUrl + "/stats/comptes");
+        response = target.request().get();
+        String rep = response.readEntity(String.class);
+        response.close();
+        return rep;
+    }
+}
